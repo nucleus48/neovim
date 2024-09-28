8c7716e3483371b577c733de027169dd9c605ab1
Nucleus Erumagborie <nucleuserumagborie@gmail.com>
2024-09-28 21:54:20 +0100

    extend graphql lsp file types

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,54 +1,61 @@
 return {
-  {
-    "williamboman/mason-lspconfig.nvim",
-    dependencies = { "williamboman/mason.nvim" },
-    opts = {
-      handlers = {
-        function(server)
-          require("lspconfig")[server].setup {
-            capabilities = require('cmp_nvim_lsp').default_capabilities()
-          }
-        end,
+	{
+		"williamboman/mason-lspconfig.nvim",
+		dependencies = { "williamboman/mason.nvim" },
+		opts = {
+			handlers = {
+				function(server)
+					require("lspconfig")[server].setup({
+						capabilities = require("cmp_nvim_lsp").default_capabilities(),
+					})
+				end,
 
-        ["jsonls"] = function()
-          require("lspconfig").jsonls.setup {
-            capabilities = require('cmp_nvim_lsp').default_capabilities(),
-            settings = {
-              json = {
-                schemas = require('schemastore').json.schemas(),
-                validate = { enable = true },
-              },
-            },
-          }
-        end,
+				["graphql"] = function()
+					require("lspconfig").graphql.setup({
+						capabilities = require("cmp_nvim_lsp").default_capabilities(),
+						filetypes = { "graphql", "typescriptreact", "javascriptreact", "typescript", "javascript" },
+					})
+				end,
 
-        ["lua_ls"] = function()
-          require("lspconfig").lua_ls.setup {
-            capabilities = require('cmp_nvim_lsp').default_capabilities(),
-            settings = {
-              Lua = {
-                diagnostics = {
-                  globals = { "vim" },
-                },
-                workspace = {
-                  library = {
-                    [vim.fn.expand "$VIMRUNTIME/lua"] = true,
-                    [vim.fn.expand "$VIMRUNTIME/lua/vim/lsp"] = true
-                  },
-                  maxPreload = 100000,
-                  preloadFileSize = 10000,
-                },
-              },
-            },
-          }
-        end
-      }
-    }
-  },
+				["jsonls"] = function()
+					require("lspconfig").jsonls.setup({
+						capabilities = require("cmp_nvim_lsp").default_capabilities(),
+						settings = {
+							json = {
+								schemas = require("schemastore").json.schemas(),
+								validate = { enable = true },
+							},
+						},
+					})
+				end,
 
-  {
-    "williamboman/mason.nvim",
-    cmd = { "Mason" },
-    opts = {},
-  }
+				["lua_ls"] = function()
+					require("lspconfig").lua_ls.setup({
+						capabilities = require("cmp_nvim_lsp").default_capabilities(),
+						settings = {
+							Lua = {
+								diagnostics = {
+									globals = { "vim" },
+								},
+								workspace = {
+									library = {
+										[vim.fn.expand("$VIMRUNTIME/lua")] = true,
+										[vim.fn.expand("$VIMRUNTIME/lua/vim/lsp")] = true,
+									},
+									maxPreload = 100000,
+									preloadFileSize = 10000,
+								},
+							},
+						},
+					})
+				end,
+			},
+		},
+	},
+
+	{
+		"williamboman/mason.nvim",
+		cmd = { "Mason" },
+		opts = {},
+	},
 }
