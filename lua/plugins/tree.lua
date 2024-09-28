a767379166a216f61855b64464c0ad736a21a848
Nucleus Erumagborie <nucleuserumagborie@gmail.com>
2024-09-28 21:34:44 +0100

    increase nvim git timeout

--- lua/plugins/tree.lua
+++ lua/plugins/tree.lua
@@ -1,38 +1,36 @@
 return {
-  "nvim-tree/nvim-tree.lua",
-  lazy = false,
-  opts = {
-    renderer = {
-      root_folder_label = false,
-      highlight_git = true,
-      indent_markers = { enable = true },
-      icons = {
-        web_devicons = {
-          folder = { enable = true },
-        },
-        glyphs = {
-          git = {
-            unstaged = "",
-            staged = "S",
-            unmerged = "",
-            renamed = "",
-            untracked = "U",
-            deleted = "",
-            ignored = "◌",
-          }
-        },
-      },
-    },
-    update_focused_file = { enable = true },
-    git = {
-      enable = true,
-      show_on_dirs = true,
-      show_on_open_dirs = false,
-    },
-    diagnostics = {
-      enable = true,
-      show_on_dirs = true,
-      show_on_open_dirs = false,
-    },
-  },
+	"nvim-tree/nvim-tree.lua",
+	lazy = false,
+	opts = {
+		renderer = {
+			root_folder_label = false,
+			highlight_git = true,
+			indent_markers = { enable = true },
+			icons = {
+				glyphs = {
+					git = {
+						unstaged = "",
+						staged = "S",
+						unmerged = "",
+						renamed = "",
+						untracked = "U",
+						deleted = "",
+						ignored = "◌",
+					},
+				},
+			},
+		},
+		update_focused_file = { enable = true },
+		git = {
+			enable = true,
+			show_on_dirs = true,
+			show_on_open_dirs = false,
+			timeout = 10000,
+		},
+		diagnostics = {
+			enable = true,
+			show_on_dirs = true,
+			show_on_open_dirs = false,
+		},
+	},
 }
